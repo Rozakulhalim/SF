176c3e1816965beeafae92e2b67bac390b72aebc
Halims <78205083+Rozakulhalim@users.noreply.github.com>
2021-05-04 13:07:28 +0700

    # v 1.1
    Limz

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,6 +25,6 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """No one can help you buddy expect yourself, for any help/support/issue contact my master Softfreakz!""",
+        """No one can help you buddy expect yourself, for any help/support/issue contact my master [Limz77](buttonurl://t.me/MRojeck_Lim)!""",
         disable_web_page_preview=True,
     )
